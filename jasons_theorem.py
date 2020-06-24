a10f090ab6a2b18b6ff9c8cf246db5a6fc0a2ec2
jasonl0209 <48419911+jasonl0209@users.noreply.github.com>
2020-06-23 21:59:24 -0400

    Update jasons_theorem.py

--- jasons_theorem.py
+++ jasons_theorem.py
@@ -1,16 +1,38 @@
+#Author: Jason Lu (DMOJ Account: dmoj.ca/user/hfanalytica)
+#Problem Link: dmoj.ca/problem/seq1
+#Tags: Intermediate Math
+#Concepts: Binary Exponentiation
+#Time Complexity: O(log(N)), Space Complexity: O(1 
+
+"""
+Problem Analysis:
+
+Let's observe the first few cases: N = [0, 1, 2, 3, 4] & K(N) = [3, 11, 27, 59, 123], wehere K(N) is the number of keystrokes for N 
+Finding the first differences yield: [8, 16, 32, 64]; these are consecutive powers of 2. 
+It is then reasonable to conclude that K(N)=  2^(N±X) - Y
+Through further testing, we find this following pattern: K(N) = 2^(N+3) - 5
+Therefore, the output is: 2^(N+3) - 5 mod 10^9 + 7
+
+Implementation Analysis:
+
+This seems straight forward; however, in order for the program to run under 0.42 seconds per case, with max(N) < 2^63, it cannot be fully solved through brute force exponentiation, since that runs in O(N) time. 
+A neat trick with Python would be using the pow() function for quick exponentiation. This works; however, it still is not the most efficient solution. 
+We could instead choose to implement an efficient binary exponentiation function, as shown below.
+
+"""
 import sys
-mod = 1000000007
-def exp_squaring(a,n):
-    if n == 0:
+modulo = 1000000007
+def binary_exponentiation(base, exponent, mod):
+    if exponent == 0:
         return 1 
-    elif n == 1:
-        return a 
+    elif exponent == 1:
+        return base
     else:
-        split = exp_squaring(a,n//2)
-        if n%2 == 0:
-            return (split * split)%mod 
+        split = binary_exponentiation(base, exponent//2, modulo)
+        if exponent % 2 == 0:
+            return (split * split) % mod 
         else:
-            return (split * split * a)%mod
-n = int(sys.stdin.readline())
-j = exp_squaring(2,n+3) - 5
-print(j%mod)
+            return (split * split * base) % mod
+N = int(sys.stdin.readline())
+keystrokes = (binary_exponentiation(2, N+3, modulo) - 5) % modulo
+print(keystrokes)
